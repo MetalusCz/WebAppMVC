3483dbcf5f2808e39fa00f92d9ca6e971dae56fe
David <hejna.david@seznam.cz>
2023-02-06 21:42:20 +0100

    first commit here

---
+++ SchoolWebApp/obj/Release/net6.0/PubTmp/EFSQLScripts/SchoolWebApp.Data.ApplicationDbContext.sql
@@ -0,0 +1,99 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026073047_init')
+BEGIN
+    CREATE TABLE [Students] (
+        [Id] int NOT NULL IDENTITY,
+        [FirstName] nvarchar(max) NOT NULL,
+        [LastName] nvarchar(max) NOT NULL,
+        [DateOfBirth] datetime2 NOT NULL,
+        CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221026073047_init')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221026073047_init', N'6.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031071658_studentsModelAdded')
+BEGIN
+    CREATE TABLE [Subjects] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NOT NULL,
+        CONSTRAINT [PK_Subjects] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031071658_studentsModelAdded')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221031071658_studentsModelAdded', N'6.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031083347_gradesTableAdded')
+BEGIN
+    CREATE TABLE [Grades] (
+        [Id] int NOT NULL IDENTITY,
+        [StudentId] int NOT NULL,
+        [SubjectId] int NOT NULL,
+        [What] nvarchar(max) NOT NULL,
+        [Mark] int NOT NULL,
+        [Date] datetime2 NOT NULL,
+        CONSTRAINT [PK_Grades] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Grades_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Grades_Subjects_SubjectId] FOREIGN KEY ([SubjectId]) REFERENCES [Subjects] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031083347_gradesTableAdded')
+BEGIN
+    CREATE INDEX [IX_Grades_StudentId] ON [Grades] ([StudentId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031083347_gradesTableAdded')
+BEGIN
+    CREATE INDEX [IX_Grades_SubjectId] ON [Grades] ([SubjectId]);
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20221031083347_gradesTableAdded')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20221031083347_gradesTableAdded', N'6.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
